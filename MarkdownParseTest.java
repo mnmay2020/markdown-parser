2f537ad86bdb38472dd805cd9aeb4f65163df8f7
mnmay2020 <103155657+mnmay2020@users.noreply.github.com>
2022-05-30 15:56:03 -0700

    updated

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,25 +1,81 @@
 import static org.junit.Assert.*;
-import org.junit.*;
+
+import java.beans.Transient;
 import java.io.IOException;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
-public class MarkdownParseTest {
+import java.util.*;
 
-    
+import org.junit.*;
+
+public class MarkdownParseTest {
+    private Path fileName;
+    private String content;
+    private ArrayList<String> links;
+    private ArrayList<String> result;
+    private static final String FULL_DIRECTORY = "C:\\Users\\ebuen\\OneDrive\\Documents\\GitHub\\markdown-parser-clone\\";
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+        assertEquals(5, 2 + 3);
     }
 
     @Test
-    public void checkLinks() throws Exception{
-        Path fileName = Path.of("test-file.md");
-        String content = Files.readString(fileName);
-        List<String> links = MarkdownParse.getLinks(content);
-        List<String> expected = List.of("https://something.com", "some-page.html");
-        assertEquals(expected, links);
+    public void getLinksTest(){
+        try{
+            fileName = Path.of(FULL_DIRECTORY + "test-file.md");
+            content = Files.readString(fileName);
+            links = MarkdownParse.getLinks(content);
+            result = new ArrayList<>(List.of("https://something.com", "some-thing.html"));
+        }
+        catch(IOException e){
+
+        }
+        assertEquals(result, links);
+    }
+
+    @Test
+    public void testSnippet1(){
+        try{
+            fileName = Path.of(FULL_DIRECTORY + "snippet1-test.md");
+            content = Files.readString(fileName);
+            links = MarkdownParse.getLinks(content);
+            result = new ArrayList<>(List.of("google.com"));
+        }
+        catch(IOException e){
+
+        }
+        assertEquals(result, links);
+    }
+
+    @Test
+    public void testSnippet2(){
+        try{
+            fileName = Path.of(FULL_DIRECTORY + "snippet2-test.md");
+            content = Files.readString(fileName);
+            links = MarkdownParse.getLinks(content);
+            result = new ArrayList<>(List.of("a.com", "a.com(())", "example.com"));
+        }
+        catch(IOException e){
+
+        }
+        assertEquals(result, links);
+    }
+
+    @Test
+    public void testSnippet3(){
+        try{
+            fileName = Path.of(FULL_DIRECTORY + "snippet3-test.md");
+            content = Files.readString(fileName);
+            links = MarkdownParse.getLinks(content);
+            result = new ArrayList<>(List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule"));
+        }
+        catch(IOException e){
+
+        }
+        assertEquals(result, links);
     }
 }
